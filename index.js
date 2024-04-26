6aa5483f2204897cc9836144f4b6dca7e1972304
Parth-11 <parth12.mgg@gmail.com>
2024-04-26 10:42:37 +0530

    Added backend index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`);
+});
